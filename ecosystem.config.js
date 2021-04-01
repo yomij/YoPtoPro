76b83db1bed811fc0d252efa6f35230bef34f622
yomi <yomi_Sheng@163.com>
2021-04-01 18:09:46 +0800

    style: format code

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  apps : [{
+  apps: [{
     script: 'app.js',
     watch: '.'
   }, {
     script: './service-worker/',
     watch: ['./service-worker']
   }],
-
-  deploy : {
-    production : {
-      user : 'root',
-      host : ["yomij.cn"],
-      "port": "22",
-      ref  : 'origin/master',
-      repo : 'git@github.com:yomij/YoPtoPro.git',
-      path : '/root/yomi',
+  
+  deploy: {
+    production: {
+      user: 'root',
+      host: ["yomij.cn"],
+      port: "22",
+      ref: 'origin/master',
+      repo: 'git@github.com:yomij/YoPtoPro.git',
+      path: '/root/yomi',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
