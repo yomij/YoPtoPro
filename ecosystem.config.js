2fb0f152006dee72e1bca09c5e1bacc98fa82790
shengyongming <shengyongming.sym@alibaba-inc.com>
2021-09-01 19:35:03 +0800

    chore: update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,12 +6,16 @@ module.exports = {
   }],
   deploy: {
     production: {
-      user: 'root',
-      host: ["yomij.com"],
-      port: "22",
-      ref: 'origin/master',
-      repo: 'git@github.com:yomij/YoPtoPro.git',
-      path: '/root/yomi',
+      key: '~/.ssh/pm2deploy',
+      user: 'yomi',
+      host: [{
+        host: '121.4.172.117',
+        port: "22",
+      }],
+      ref: 'origin/dev',
+
+      repo: 'https://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@github.com/yomij/YoPtoPro.git',
+      path: 'project',
       'pre-deploy-local': '',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
